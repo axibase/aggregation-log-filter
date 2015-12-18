10ee2cb41da8f1e0fd3d7247b1449d69dba08e3c
Nikolay Malevanny <zwave@ya.ru>
2015-12-18 15:46:04 +0300

    #2017 fixed; added debug property to log commands

---
+++ src/main/java/com/axibase/tsd/collector/writer/LoggingWrapper.java
@@ -0,0 +1,50 @@
+package com.axibase.tsd.collector.writer;
+
+import com.axibase.tsd.collector.AtsdUtil;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * @author Nikolay Malevanny.
+ */
+public class LoggingWrapper implements WritableByteChannel {
+    private final WritableByteChannel wrapped;
+
+    public LoggingWrapper(WritableByteChannel wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public int write(ByteBuffer src) throws IOException {
+        if (src == null) {
+            return 0;
+        }
+        ByteBuffer bbCopy = src.duplicate();
+        final byte[] b = new byte[bbCopy.remaining()];
+        bbCopy.get(b);
+        AtsdUtil.logInfo("WRITING: " + new String(b));
+        final int written = wrapped.write(src);
+        AtsdUtil.logInfo(("WRITTEN: " + written));
+        return written;
+    }
+
+    @Override
+    public boolean isOpen() {
+        return false;
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+
+    public static WritableByteChannel tryWrap(String debug, WritableByteChannel writer) {
+        if (debug != null && ("1".equals(debug) || "true".equals(debug) || "TRUE".equals(debug))) {
+            return new LoggingWrapper(writer);
+        } else {
+            return writer;
+        }
+    }
+}
