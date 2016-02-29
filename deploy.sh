d83daa18942f64e4bef9282898378daddc4ce568
Dmitry <failsild@gmail.com>
2016-02-29 11:33:48 +0300

    deploy.sh update & version dependencies revert

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 . deploy.config
 
 mvn -s settings.xml clean javadoc:jar \
-source:jar -Dgpg.passphrase=$GPG_PASSPHRASE deploy
+source:jar -Dgpg.keyname=$GPG_KEYNAME -Dgpg.passphrase=$GPG_PASSPHRASE deploy
