39548a2f7fe4a5f184cef00b7a3c951a5e71304c
rmakulov <Corolla789>
2016-03-08 13:17:37 +0300

    #2321

--- src/test/java/com/axibase/tsd/collector/log4j/Log4jMessageWriterTest.java
+++ src/test/java/com/axibase/tsd/collector/log4j/Log4jMessageWriterTest.java
@@ -43,7 +43,7 @@ public class Log4jMessageWriterTest {
         messageBuilder.writeStatMessages(catcher, events, 60000);
         String result = catcher.sb.toString();
         System.out.println("result = " + result);
-        assertTrue(result.substring(0, result.indexOf("ms:1")).contains(
+        assertTrue(result.substring(0, result.length()).contains(
                 "t:ttt1=vvv1 t:ttt2=vvv2 m:log_event_counter=100 t:level=ERROR t:logger=test-logger"));
     }
 
@@ -127,8 +127,8 @@ public class Log4jMessageWriterTest {
         StringsCatcher catcher = new StringsCatcher();
         messageBuilder.writeSingles(catcher, createSingles(event, 0));
         String result = catcher.sb.toString();
-        assertTrue(result.substring(0, result.indexOf("ms:1")).contains(
-                "t:ttt1=vvv1 t:ttt2=vvv2 t:type=logger m:test-message t:severity=ERROR t:level=ERROR t:source=test-logger "));
+        assertTrue(result.substring(0, result.length()).contains(
+                "t:ttt1=vvv1 t:ttt2=vvv2 t:type=logger m:test-message t:severity=ERROR t:level=ERROR t:source=test-logger"));
     }
 
     @Test
