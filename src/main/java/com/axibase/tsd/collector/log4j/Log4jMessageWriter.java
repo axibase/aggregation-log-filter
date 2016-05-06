8b4eeed584f1791a0ce9031b8a4f4b9a8842d17f
Rustam Makulov <r.n.makulov@gmail.com>
2016-05-06 16:14:42 +0300

    #2694

--- src/main/java/com/axibase/tsd/collector/log4j/Log4jMessageWriter.java
+++ src/main/java/com/axibase/tsd/collector/log4j/Log4jMessageWriter.java
@@ -207,7 +207,7 @@ public class Log4jMessageWriter implements MessageWriter<LoggingEvent, String, S
                             Level.toLevel(l).toString());
                 }
             } catch (IOException e) {
-                AtsdUtil.logError("Writer failed to send initial total counter value for " + Level.toLevel(level));
+                AtsdUtil.logInfo("Writer failed to send initial total counter value for " + Level.toLevel(level));
             }
         }
 
