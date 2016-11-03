c3e0776eca56309eb2b7fe2196b4eea69a2d431c
rmakulov <rustam.makulov@axibase.com>
2016-11-03 21:06:16 +0300

    mv logs from stdout to AtsdUtil

--- src/main/java/com/axibase/tsd/collector/log4j/Log4jMessageWriter.java
+++ src/main/java/com/axibase/tsd/collector/log4j/Log4jMessageWriter.java
@@ -223,7 +223,7 @@ public class Log4jMessageWriter implements MessageWriter<LoggingEvent, String, S
             CounterWithSum total = new CounterWithSum(0, seriesSenderConfig.getRepeatCount());
             totals.put(Level.toLevel(l).toString(), total);
         }
-        System.out.println("Aggregation log filter: connecting to ATSD on " + atsdUrl);
+        AtsdUtil.logInfo("Aggregation log filter: connecting to ATSD on " + atsdUrl);
         if (writer != null) {
             try {
                 for (int l : levels) {
@@ -238,12 +238,12 @@ public class Log4jMessageWriter implements MessageWriter<LoggingEvent, String, S
                     writerToCheck = ((LoggingWrapper) writerToCheck).getWrapped();
                 }
                 if (writerToCheck instanceof TcpAtsdWriter)
-                    System.out.println("Aggregation log filter: connected to ATSD.");
+                    AtsdUtil.logInfo("Aggregation log filter: connected to ATSD.");
                 else if (writerToCheck instanceof BaseHttpAtsdWriter) {
-                    System.out.println("Aggregation log filter: connected with status code " + ((BaseHttpAtsdWriter) writerToCheck).getStatusCode());
+                    AtsdUtil.logInfo("Aggregation log filter: connected with status code " + ((BaseHttpAtsdWriter) writerToCheck).getStatusCode());
                 }
             } catch (IOException e) {
-                System.out.println("Aggregation log filter: failed to connect to ATSD.");
+                AtsdUtil.logInfo("Aggregation log filter: failed to connect to ATSD.");
                 AtsdUtil.logInfo("Writer failed to send initial total counter value for " + Level.toLevel(level));
             }
         }
